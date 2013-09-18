d6741d72e9c9fb6ebd4e3545088e943fba85f419
Caio Ariede <caio.ariede@gmail.com>
2013-09-18 18:50:26 -0300

    Consolidated tokenizer

--- framework/nimtpl.nim
+++ framework/nimtpl.nim
@@ -1,36 +1,54 @@
-from strutils import startsWith
-from pegs import parsePeg, findAll, TPeg
+from strutils import startsWith, `%`
+from re import re, split, escapeRe, findBounds
+
+
+const
+    blockTagStart = "{%"
+    blockTagEnd = "%}"
+    commentTagStart = "{#"
+    commentTagEnd = "#}"
+    variableTagStart = "{{"
+    variableTagEnd = "}}"
+
+
+let regexTag = re("($1.*?$2|$3.*?$4|$5.*?$6)" % [
+        escapeRe(blockTagStart), escapeRe(blockTagEnd),
+        escapeRe(commentTagStart), escapeRe(commentTagEnd),
+        escapeRe(variableTagStart), escapeRe(variableTagEnd)])
 
 
 type
     Token = ref object {.inheritable.}
-        name: string
         content: string
         line: int
     TextToken = ref object of Token
-    TagToken = ref object of Token
-    FilterToken = ref object of Token
+    BlockToken = ref object of Token
     CommentToken = ref object of Token
+    VariableToken = ref object of Token
 
 
 iterator tokenize(content: string): Token =
-    let grammar = parsePeg("""
-        grammar <- tag
-        tag <- "{%" @ "%}" / comment
-        comment <- "{#" @ "#}" / filter
-        filter <- "{{" @ "}}" / text
-        text <- [^{]+ &"{" / .+
-""", "template.html", line=1, col=0)
+    var matches: array[0..1, tuple[first, last: int]]
+    var start: int
+    var slice: string
 
-    for match in findAll(content, grammar, start=0):
-        if match.startsWith("{%"):
-            yield TagToken(name: "tag", content: match, line: 0)
-        elif match.startsWith("{#"):
-            yield CommentToken(name: "comment", content: match, line: 0)
-        elif match.startsWith("{{"):
-            yield FilterToken(name: "filter", content: match, line: 0)
+    while True:
+        if findBounds(content, regexTag, matches, start).first < 0:
+            break
         else:
-            yield TextToken(name: "text", content: match, line: 0)
+            for match in matches:
+                if match.last > 0:
+                    if match.first > 0:
+                        slice = content[start..match.first - 1]
+                        yield TextToken(content: slice, line: 0)
+                    slice = content[match.first..match.last]
+                    if slice.startsWith("{%"):
+                        yield BlockToken(content: slice, line: 0)
+                    elif slice.startsWith("{#"):
+                        yield CommentToken(content: slice, line: 0)
+                    else:
+                        yield VariableToken(content: slice, line: 0)
+                    start = match.last + 1
 
 
 method parse(token: Token) =
@@ -41,12 +59,12 @@ method parse(token: TextToken) =
     echo("Text: " & token.content)
 
 
-method parse(token: TagToken) =
-    echo("Tag: " & token.content)
+method parse(token: BlockToken) =
+    echo("Block: " & token.content)
 
 
-method parse(token: FilterToken) =
-    echo("Filter: " & token.content)
+method parse(token: VariableToken) =
+    echo("Variable: " & token.content)
 
 
 method parse(token: CommentToken) =
@@ -56,6 +74,6 @@ method parse(token: CommentToken) =
 for token in tokenize("""<ul>
     {% for x in list %}
         <li>{{ x }}</li>
-    {% endfor %
+    {% endfor %}
 </ul>"""):
     parse(token)
